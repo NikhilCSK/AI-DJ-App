6e55491e109abed3ea9715f62c8499e128ec0ca6
NikhilCSK <102224467+NikhilCSK@users.noreply.github.com>
2023-04-04 19:15:50 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 song = "";
 leftWristX = 0;
 leftWristY = 0;
+
 rightWristX = 0;
 rightWristY = 0;
+
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 
 function preload()
@@ -59,6 +62,8 @@ function gotPoses(results)
                 
                 console.log("Right Wrist X = " + rightWristX + " and Right Wrist Y = " + rightWristY );
                 
+                scoreRightWrist =  results[0].pose.keypoints[10].score;
+                console.log("Score of Right Wrist is " + scoreRightWrist); 
             }
         
 
@@ -82,6 +87,52 @@ function draw()
         setVolume(volume);
         document.getElementById("volume").innerHTML = "Volume = " + volume;
 
+    }
+
+    if (scoreRightWrist > 0.2)
+    {
+
+        circle(rightWristX, rightWristY, 20);
+
+        if (rightWristY > 0 && rightWristY <= 100)
+        {
+
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+
+        }
+
+        else if ( rightWristY > 100 && rightWristY <= 200)
+        {
+
+            document.getElementById("speed").innerHTML = "Speed = 1x";
+            song.rate(1);
+
+        }
+
+        else if ( rightWristY > 200 && rightWristY <=300)
+        {
+
+            document.getElementById("speed").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+
+        }
+
+        else if (rightWristY > 300 && rightWristY <= 400)
+        {
+
+            document.getElementById("speed").innerHTML = "Speed = 2x";
+            song.rate(2);
+
+        }
+
+        else if(rightWristY > 400 && rightWristY <= 500)
+        {
+
+            document.getElementById("speed").innerHTML = "Speed = 2.5x";
+            song.rate(2.5);
+
+        }
     }
 
 }
